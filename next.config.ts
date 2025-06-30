e9ff7afefb36d377a80c7e8b33bf2fba05cca15d
Clave Sarmiento <clavesarmiento@gmail.com>
2025-06-30 20:12:20 +0800

    feat(quiz): next pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+// @ts-expect-error: No types available for next-pwa
+import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
